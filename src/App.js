54408c281356bc551e6410d69083605a6dde4171
Maksym <maxthebest1906@gmail.com>
2023-03-02 16:58:46 +0200

    search variants and assertive functions

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
+
+const getUser = () => Promise.resolve({ id: 1, name: "user1" }); 
 
 const Search = ({ value, onChange, children }) => (
   <div>
     <label htmlFor="search">{children}</label>
-    <input id="search" type="text" value={value} onChange={onChange} placeholder="enter search query"/>
+    <input 
+    id="search" 
+    type="text" 
+    value={value} 
+    onChange={onChange} 
+    placeholder="enter search query"
+    required
+    />
   </div>
 );
 
 const App = () => {
   const [search, setSearch] = useState("");
+  const [user, setUser] = useState("");
+  useEffect( () => {
+    const loadUser = async () => {
+      const res = await getUser();
+      setUser(res);
+    }
+    loadUser();
+  }, []);
 
   const handleChange = ({ target }) => {
     setSearch(target.value);
@@ -18,7 +35,8 @@ const App = () => {
 
   return (
     <div>
-      <img src="" alt="search icon" />
+      {user && <h2>Logged in as {user.name}</h2>}
+      <img src="" alt="search icon" className="search-img" />
       <Search value={search} onChange={handleChange}>
         SEARCH:
       </Search>
--- src/App.test.js
+++ src/App.test.js
@@ -2,13 +2,27 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe("App", () => {
-  it("renders App component", () => {
+  it('renders App component', () => {
     render(<App />);
-    expect(screen.getByText(/Search:/i)).toBeInTheDocument(); //get label by text
-    expect(screen.getByRole('textbox')).toBeInTheDocument();  //get input by role
-    expect(screen.getByLabelText(/search/i)).toBeInTheDocument(); //get input by labael text
-    expect(screen.getByPlaceholderText('enter search query')).toBeInTheDocument(); //get input by placeholder text
-    expect(screen.getByAltText('search icon')).toBeInTheDocument(); //get img by alt text
-    // expect(screen.getAllByDisplayValue('')).toBeInTheDocument(); //get imput (or another form field) by value attribute static string - initial value
+    expect(screen.queryByText(/searches for react/i)).toBeNull(); // markup has no such element
+  });
+  it('async user fetch', async () => {
+    render(<App />);
+    expect(screen.queryByText(/Logged in as/i)).toBeNull();                // check if markup has no element with this text
+    // screen.debug();                                                    // can see that markup has no h2 tag
+    expect(await screen.findByText(/Logged in as/i)).toBeInTheDocument(); // then after await async operation we check that markup has element with this text
+    // screen.debug();                                                    // can see that markup has h2 tag with user name after user fetching
+  });
+  it('find img with class', () => {
+    render(<App />);
+    expect(screen.getByAltText(/search icon/i)).toHaveClass('search-img'); // get img with this alt text and check class 'search-img'
+  });
+  it('input tests', () => {
+    render(<App />);
+    // expect(screen.getByLabelText(/search:/i)).not.toBeRequired();        // input has no required attr
+    expect(screen.getByLabelText(/search:/i)).toBeRequired();               // get input with this label text and check is it required
+    expect(screen.getByLabelText(/search:/i)).toBeEmptyDOMElement();        // check if input is empty (toBeEmpty - deprecated)
+    expect(screen.getByLabelText(/search:/i)).toHaveAttribute('id');        // check if input has attr id 
+    expect(screen.getByLabelText(/search:/i)).not.toHaveStyle('color: red');// check input has no style 'color: red'
   });
 });
